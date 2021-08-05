1c2ddbc592b799b6e3fbec819062f898169d1802
Ryan Callahan <RyanCallahan312@gmail.com>
2021-08-05 10:58:55 -0400

    Project Initalizr
    using:
    - gradle
    - java 16

---
+++ src/main/java/com/azware/missingpersons/MissingPersonsApplication.java
@@ -0,0 +1,13 @@
+package com.azware.missingpersons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissingPersonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissingPersonsApplication.class, args);
+	}
+
+}
